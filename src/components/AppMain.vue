cd08886a78470b96afe7fa2186469c2ec817b864
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-20 16:43:33 +0200

    .

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,13 @@
 <template>
     <main class="d-flex flex-wrap justify-content-center">
-        <div v-for="(film, index) in films" :key="`film-${index}`" class="card-sc">
-        <img :src="`https://image.tmdb.org/t/p/w500${film.poster_path}`" alt="">
-        <p>Titolo: {{film.title}}</p>
-        <p>Titolo originale: {{film.original_title}}</p>
-        <p>Lingua: {{film.original_language}}</p>
-        <p>Voto: {{film.vote_average}}</p>
+        <div v-for="(film, index) in films" :key="`film-${index}`" class="card-sc ">
+            <img :src="`https://image.tmdb.org/t/p/w500${film.poster_path}`" alt="">
+            <div class="card-info">
+                <p>Titolo: {{film.title}}</p>
+                <p>Titolo originale: {{film.original_title}}</p>
+                <p>Lingua: {{film.original_language}}</p>
+                <p>Voto: {{film.vote_average}}</p>
+            </div>
         </div>
     </main>
 </template>
@@ -30,9 +32,25 @@ export default {
         justify-content: center;
         margin: 30px;
         overflow: hidden;
+        position: relative;
         img{
           width: 100%;
         }
+         .card-info{
+          position: absolute;
+          top: 0;
+          left:0;
+          margin: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.641);
+          padding: 20px;
+          text-align: center;
+          display: none;
+        }
+         &:hover .card-info{
+            display: block;
+          }
 }
 
 </style>
