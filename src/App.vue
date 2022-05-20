07e237d18323cd5b0bea58a9b759aab14b5bbd69
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-20 11:36:47 +0200

    created api conection going to main

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <AppHeader />
+    <div v-if="loader" class="text-center">
+      <div class="lds-ring "><div></div><div></div><div></div><div></div></div>
+    </div>
   </div>
 </template>
 
@@ -12,19 +15,24 @@ export default {
     name: "App",
     data() {
         return {
-            apiUrl: "https://api.themoviedb.org/3/",
+            apiUrl: "https://api.themoviedb.org/3/search/movie",
             apiStructure:{
-              api_key: "2a58d3ca96348e8aa76ec66be55ffce4"
-            }
+              api_key: "2a58d3ca96348e8aa76ec66be55ffce4",
+              language: "it-IT",
+              query:"potter"
+            },
+            loader: true,
         };
     },
     methods: {
       getApi(){
+         this.loader = true
         axios.get(this.apiUrl,{
           params:this.apiStructure
         })
         .then(r=>{
-          console.log(r)
+          console.log(r.data.results)
+          this.loader = false
         })
         .catch(err=>{
           console.log("LOL!" ,err)
@@ -40,5 +48,39 @@ export default {
 
 <style lang="scss">
 @import "./assets/style/global";
-
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #a00f0f;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #a00f0f transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
