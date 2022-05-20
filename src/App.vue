01609e6dd83e02c5556136468a70a58751e47c52
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-20 11:27:56 +0200

    created nav going to api requests

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
+    <AppHeader />
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  
+    name: "App",
+    data() {
+        return {
+            apiUrl: "https://api.themoviedb.org/3/",
+            apiStructure:{
+              api_key: "2a58d3ca96348e8aa76ec66be55ffce4"
+            }
+        };
+    },
+    methods: {
+      getApi(){
+        axios.get(this.apiUrl,{
+          params:this.apiStructure
+        })
+        .then(r=>{
+          console.log(r)
+        })
+        .catch(err=>{
+          console.log("LOL!" ,err)
+        })
+      }
+    },
+    mounted() {
+      this.getApi()
+    },
+    components: { AppHeader }
 }
 </script>
 
 <style lang="scss">
+@import "./assets/style/global";
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="pt-5 pb-5">
+    <div class="container d-flex justify-content-between">
+      <div class="logo">Boolflix</div>
+      <input v-model="searchkey" placeholder="cerca" type="text">
+    </div>
+      
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      searchkey: ""
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
