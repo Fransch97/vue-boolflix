85f050e9ef7e7a4eb314e3667ed50d2bca24ff48
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-20 11:54:55 +0200

    milestone 1 finish

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,10 @@
 <template>
-    <main>
-        <div v-for="(film, index) in films" :key="`film-${index}`">
-        <p>{{film.title}}</p>
-        <p>{{film.original_title}}</p>
-        <p>{{film.original_language}}</p>
-        <p>{{film.vote_average}}</p>
+    <main class="d-flex flex-wrap justify-content-center">
+        <div v-for="(film, index) in films" :key="`film-${index}`" class="d-flex">
+        <p>Titolo: {{film.title}}</p>
+        <p>Titolo originale: {{film.original_title}}</p>
+        <p>Lingua: {{film.original_language}}</p>
+        <p>Voto: {{film.vote_average}}</p>
         </div>
     </main>
 </template>
@@ -19,6 +19,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+main{
+    div{
+        background-color: rgb(55, 55, 55);
+        height: 300px;
+        width: 300px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: 30px;
+        padding: 40px;
+    }
+}
 
 </style>
